d76040dbc26ee34ac090602326884e9634cf756a
Your Name <you@example.com>
2024-07-14 14:57:21 -0700

    fixing errors for build

--- app/api/ticket/new/route.ts
+++ app/api/ticket/new/route.ts
@@ -5,23 +5,21 @@ import { TicketModel } from "@/schemas/ticket";
 import { getDownloadURL, uploadBytes } from "firebase/storage";
 import { NextRequest, NextResponse } from "next/server";
 
-
-export async function POST(request: NextRequest, response: NextResponse) {
-
+export async function POST(request: NextRequest) {
     try {
-        await connectToDB()
+        await connectToDB();
 
-        const formData: FormData = await request.formData()
+        const formData: FormData = await request.formData();
 
-        const userInfo = formData.get('userinfo') as string
-        const userJson = JSON.parse(userInfo)
+        const userInfo = formData.get('userinfo') as string;
+        const userJson = JSON.parse(userInfo);
         let downloadUrl = '';
         const submitterName = userJson.submitterName;
         const submitterPhone = userJson.submitterPhone;
         const submitterEmail = userJson.submitterEmail;
         const lat = userJson.latlong.lat;
         const long = userJson.latlong.long;
-        const file = formData.get('image') as File
+        const file = formData.get('image') as File;
 
         let ticketToSave = {
             submitterName: submitterName,
@@ -29,18 +27,18 @@ export async function POST(request: NextRequest, response: NextResponse) {
             submitterEmail: submitterEmail,
             photo: '',
             latlong: {
-              coordinates: [lat,long]
+                coordinates: [lat, long]
             }
-        }
+        };
 
-        if( file ) {
-            const filename = Date.now() + '.' + file.name.split('.').pop()
-            const ref = storageRef(`${filename}`)
+        if (file) {
+            const filename = Date.now() + '.' + file.name.split('.').pop();
+            const ref = storageRef(`${filename}`);
 
-            const buffer = Buffer.from(await file.arrayBuffer())
-            await uploadBytes(ref, buffer)
-            downloadUrl = await getDownloadURL(ref)
-            ticketToSave['photo'] = downloadUrl
+            const buffer = Buffer.from(await file.arrayBuffer());
+            await uploadBytes(ref, buffer);
+            downloadUrl = await getDownloadURL(ref);
+            ticketToSave['photo'] = downloadUrl;
         }
 
         // save the ticket
@@ -49,11 +47,11 @@ export async function POST(request: NextRequest, response: NextResponse) {
         return NextResponse.json({
             message: "Ticket created",
             ticket: ticketToSave
-        })
+        });
     } catch (error) {
-        console.log(error)
-        return Response.json({
+        console.log(error);
+        return NextResponse.json({
             message: "Something went wrong"
-        })
+        });
     }
 }
