84ca8d97a34bf1a9f308b380cdca85834521ae4f
Your Name <you@example.com>
2024-07-15 22:52:57 -0700

    not stable at all

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -48,6 +48,7 @@ const DialogContent = React.forwardRef<
         "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full",
         className
       )}
+      aria-describedby="dialog-description"
       {...props}
     >
       {children}
@@ -114,6 +115,7 @@ const DialogDescription = React.forwardRef<
   <DialogPrimitive.Description
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
+    id="dialog-description"
     {...props}
   />
 ))
@@ -132,3 +134,18 @@ export {
   DialogTitle,
   DialogDescription,
 }
+
+// Usage within Dialog component
+const MyDialog = () => (
+  <Dialog>
+    <DialogTrigger>Open Dialog</DialogTrigger>
+    <DialogContent>
+      <DialogDescription>
+        This is the description of the dialog.
+      </DialogDescription>
+      {/* Other dialog content */}
+    </DialogContent>
+  </Dialog>
+);
+
+export default MyDialog;
